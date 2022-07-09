e09e228d99bbcfa0e26d2634161cfddc226fa1e9
Mike <ampgzo2@gmail.com>
2022-07-09 11:04:06 -0400

    create style.css and header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div
+        className='page-hero'
+        xyz='fade small stagger ease-out-back duration-30'
+    >
+        <div className='hero-logo xyz-nested'></div>
+        <p className='hero-text xyz-nested'>
+            Curabitur blandit tempus porttitor. Morbi leo risus. 
+        </p>
+
+    </div>
+  )
+}
+
+export default Header
