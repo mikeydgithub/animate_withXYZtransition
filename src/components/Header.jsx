edb21a5bdd1aff9dd17dd34da2d67a68ba4bd5b5
Mike <ampgzo2@gmail.com>
2022-07-09 11:27:57 -0400

    finish header and create body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div
-        className='page-hero'
-        xyz='fade small stagger ease-out-back duration-30'
-    >
-        <div className='hero-logo xyz-nested'></div>
-        <p className='hero-text xyz-nested'>
-            Curabitur blandit tempus porttitor. Morbi leo risus. 
+export default function Header() {
+    return (
+      <div
+        className="page-hero"
+        xyz="fade small stagger ease-out-back duration-30"
+      >
+        <div className="hero-logo xyz-nested"></div>
+        <p className="hero-text xyz-nested">
+          Curabitur blandit tempus porttitor. Morbi leo risus.
         </p>
+      </div>
+    );
+} 
 
-    </div>
-  )
-}
-
-export default Header
+// styles
+// fade: animates an emelement's opacity to fade it out of visibility
+// small: scales an element down along the x,y, and z-axis
+// stagger: adds a delay to each child element so that individual animations execute in sequence instead of at the same time
+// ease-out-back: changes the speed curve of the animation and adds a slight overshoot to its end
+// duration: sets the length of the animation. 
